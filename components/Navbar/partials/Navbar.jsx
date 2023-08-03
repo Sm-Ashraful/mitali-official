8bee24ac8adc6d0241b03cf3697ba812f56cc74b
mitali-international-limited <124981715+mitali-international-limited@users.noreply.github.com>
2023-08-03 16:27:24 +0600

    font

--- components/Navbar/partials/Navbar.jsx
+++ components/Navbar/partials/Navbar.jsx
@@ -4,7 +4,7 @@ import { AiOutlineCaretDown } from "react-icons/ai";
 const Navbar = ({ className }) => {
   return (
     <ul
-      className={`font-Allerta gap-5 xl:gap-12 pl-6 xl:pl-10 text-white font-[500] xl:text-[26px] ${className}`}
+      className={`font-Allerta gap-5 xl:gap-12 pl-6 xl:pl-10 text-white font-[500] 2xl:text-[26px] ${className}`}
     >
       <li>Home</li>
       <li>Our Profile</li>
