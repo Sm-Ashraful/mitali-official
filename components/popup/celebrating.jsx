cf1cd66833f5974d36ebb902de252bee9f49870d
Sm-Ashraful <smashraful.dev@gmail.com>
2023-10-10 14:35:24 +0600

    wesite spped optimization

--- components/popup/celebrating.jsx
+++ components/popup/celebrating.jsx
@@ -11,12 +11,15 @@ const PopUpCelebrate = ({ isOpen, onClose, mobileView }) => {
       {isOpen && (
         <div className="w-[95%]  fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[9999]">
           <div className="relative w-full md:w-3/4 mx-auto bg-white p-4 rounded-lg shadow-lg flex justify-center items-center ">
-            <div className="relative h-[360px] w-full">
-              {mobileView ? (
-                <Image src="/Image/9yrsm.jpeg" alt="clcebrate" fill cover />
-              ) : (
-                <Image src="/Image/9yrs.jpeg" alt="celebrate" fill cover />
-              )}
+            <div className="relative w-full">
+              <Image
+                src="/Image/9yrs.jpeg"
+                alt="celebrate"
+                priority={true}
+                width={1200}
+                height={366}
+                cover
+              />
             </div>
             <div
               onClick={onClose}
