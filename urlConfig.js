497ac4159e1467d79cc9a6d4b5a09e4e111aef1d
Sm-Ashraful <smashraful.dev@gmail.com>
2023-11-16 18:43:08 +0600

    url

--- urlConfig.js
+++ urlConfig.js
@@ -1,2 +1,2 @@
-export const api = "http://localhost:8081/api";
-// export const api = "https://mitaliint.com/api";
+// export const api = "http://localhost:8081/api";
+export const api = "https://mitaliint.com/api";
